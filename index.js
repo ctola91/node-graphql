db14509c73b6c50f6a2f855933cafbf9ec1fcf75
ctola <christian.tola@truextend.com>
2020-04-16 21:20:01 -0400

    Add new object type

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const express = require('express');
+const { GraphQLSchema, GraphQLObjectType, GraphqlString, GraphQLInt , graphql } = require('graphql');
+
 
 const app = express();
 
+const courseType = new GraphQLObjectType({
+  name: 'Course',
+  fields: {
+    title: {
+      type: GraphqlString
+    },
+    views: { type: GraphQLInt}
+  }
+});
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      message: {
+        type: GraphqlString,
+        resolve() {
+          return 'Hello World';
+        }
+      },
+      course: {
+        type: courseType,
+        resolve() {
+          return { title: 'Graphql Course', views: 1000 };
+        }
+      }
+    }
+  })
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  graphql(schema, ` { message, course{ title } }`).then(r => res.json(r))
+    .catch(res.json);
 });
 
 app.listen(3000, () => {
