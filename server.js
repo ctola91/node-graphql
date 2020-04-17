f5941e69b5aefe6f5156ea10d2010113ddfcc8a8
ctola <christian.tola@truextend.com>
2020-04-17 15:45:48 -0400

    Update courses

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const { buildSchema} = require('graphql');
-const graphqlHTP = require('express-graphql')
+const express = require("express");
+const { buildSchema } = require("graphql");
+const graphqlHTP = require("express-graphql");
 // mock courses
-const courses = require('./courses');
+const courses = require("./courses");
 
 const app = express();
 
@@ -21,36 +21,48 @@ const schema = buildSchema(`
 
     type Mutation {
         addCourse(title: String!, views: Int): Course
+        updateCourse(id: ID!, title: String!, views: Int): Course
     }
 
 `);
 
 const root = {
-    getCourses() {
-        return courses;
-    },
-    getCourse({ id }) {
-        return courses.find(course => id === course.id );
-        
-    },
-    addCourse({title, views}) {
-        const id = String(courses.length + 1);
-        const course = { id, title, views };
-        courses.push(course);
-        return course;
-    }
-}
+  getCourses() {
+    return courses;
+  },
+  getCourse({ id }) {
+    return courses.find((course) => id === course.id);
+  },
+  addCourse({ title, views }) {
+    const id = String(courses.length + 1);
+    const course = { id, title, views };
+    courses.push(course);
+    return course;
+  },
+  updateCourse({ id, title, views }) {
+    const courseIndex = courses.findIndex((course) => {
+      return course.id === id;
+    });
+    const course = courses[courseIndex];
+    const newCourse = Object.assign(course, { title, views });
+    courses[courseIndex] = newCourse;
+    return newCourse;
+  },
+};
 
-app.use('/graphql', graphqlHTP({
+app.use(
+  "/graphql",
+  graphqlHTP({
     schema: schema,
     rootValue: root,
-    graphiql: true
-}))
+    graphiql: true,
+  })
+);
 
-app.get('/', (req, res) => {
-    res.json({courses});
-})
+app.get("/", (req, res) => {
+  res.json({ courses });
+});
 
 app.listen(3000, () => {
-    console.log(`Server started`);
-})
+  console.log(`Server started`);
+});
