6e4fa411dca7bc0a3daa0c48af9f47eeb01e482f
christian <christian@localhost.localdomain>
2020-04-16 22:07:28 -0400

    add mock courses

---
+++ courses.js
@@ -0,0 +1,4 @@
+module.exports = [
+    { id: '1', title: 'Graphql Course', views: 1000},
+    { id: '2', title: 'Javascript Course', views: 5000},
+];
--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const { buildSchema} = require('graphql');
+// mock courses
+const courses = require('./courses');
 
 const app = express();
+
+const schema = buildSchema(`
+    type Course {
+        id: ID!
+        title: String!
+        views: Int
+    }
+
+    type Query {
+        getCourses: [Course]
+    }
+
+`)
 
 app.get('/', (req, res) => {
     res.send('Welcome');
