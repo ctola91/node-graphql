f60be613eff9a06eda17239c9c6b359a2b661146
christian <christian@localhost.localdomain>
2020-04-16 22:40:24 -0400

    Add graphql middleware

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { buildSchema} = require('graphql');
+const graphqlHTP = require('express-graphql')
 // mock courses
 const courses = require('./courses');
 
 const app = express();
 
+//schema definition language
 const schema = buildSchema(`
     type Course {
         id: ID!
@@ -16,7 +18,19 @@ const schema = buildSchema(`
         getCourses: [Course]
     }
 
-`)
+`);
+
+const root = {
+    getCourses() {
+        return courses;
+    }
+}
+
+app.use('/graphql', graphqlHTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}))
 
 app.get('/', (req, res) => {
     res.json({courses});
