2dc2b7f67ef2e955ea191e6e76e143165d90f8a8
cmtp <ctolapacheco@gmail.com>
2020-04-19 17:55:36 -0400

    Add apollo server

--- server.js
+++ server.js
@@ -1,89 +1,10 @@
-const express = require("express");
-const { buildSchema } = require("graphql");
-const graphqlHTP = require("express-graphql");
-// mock courses
-let courses = require("./courses");
+const { ApolloServer } = require("apollo-server");
 
-const app = express();
-
-//schema definition language
-const schema = buildSchema(`
-    type Course {
-        id: ID!
-        title: String!
-        views: Int
-    }
-
-    type Alert {
-        message: String
-    }
-
-    input CourseInput {
-        title: String!
-        views: Int
-    }
-
-    type Query {
-        getCourses(page: Int, limit: Int = 1): [Course]
-        getCourse(id: ID): Course
-    }
-
-    type Mutation {
-        addCourse(input: CourseInput): Course
-        updateCourse(id: ID!, input: CourseInput): Course
-        deleteCourse(id: ID!): Alert
-    }
-
-`);
-
-const root = {
-  getCourses({ page, limit }) {
-    if (page !== undefined) {
-      return courses.slice((page - 1) * limit, page * limit);
-    }
-    return courses;
-  },
-  getCourse({ id }) {
-    return courses.find((course) => id === course.id);
-  },
-  addCourse({ input }) {
-    const id = String(courses.length + 1);
-    const course = { id, ...input };
-    courses.push(course);
-    return course;
-  },
-  updateCourse({ id, input }) {
-    const courseIndex = courses.findIndex((course) => {
-      return course.id === id;
-    });
-    const course = courses[courseIndex];
-    const newCourse = Object.assign(course, input);
-    courses[courseIndex] = newCourse;
-    return newCourse;
-  },
-  deleteCourse({ id }) {
-    courses = courses.filter((course) => {
-      course.id !== id;
-    });
-    return {
-      message: `Course with id: ${id} was deleted`,
-    };
-  },
-};
-
-app.use(
-  "/graphql",
-  graphqlHTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  })
-);
-
-app.get("/", (req, res) => {
-  res.json({ courses });
+const server = new ApolloServer({
+  schema: null,
 });
 
-app.listen(3000, () => {
-  console.log(`Server started`);
+server.listen().then(({ url }) => {
+  console.log(`Server started in ${url}`);
 });
+
